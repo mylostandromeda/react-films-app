b986ffb2d499a58ca7197c76104ee8953423f797
artur verbin <my.lost.andromeda@gmail.com>
2021-05-10 21:50:18 +0300

    fix api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,31 +2,31 @@ import apiRequest from "../helpers/apiRequest";
 
 export const login = (formData) => apiRequest({
     method: 'post',
-    url: 'api/auth/login',
+    url: '/api/auth/login',
     data: formData,
     skipAuthorization: true
 });
 
 export const register = (formData) => apiRequest({
     method: 'post',
-    url: 'api/auth/register',
+    url: '/api/auth/register',
     data: formData,
     skipAuthorization: true
 });
 
 export const getCurrentUser = () => apiRequest({
     method: 'get',
-    url: 'api/user'
+    url: '/api/user'
 });
 
 export const getUserById = (id) => apiRequest({
     method: 'get',
-    url: `api/user/${id}`
+    url: `/api/user/${id}`
 });
 
 export const getFilms = (skip, limit, name=null, filter) => apiRequest({
     method: 'get',
-    url: 'api/films',
+    url: '/api/films',
     params: {
         skip,
         limit,
@@ -37,7 +37,7 @@ export const getFilms = (skip, limit, name=null, filter) => apiRequest({
 
 export const getLikedFilms = (skip, limit) => apiRequest({
     method: 'get',
-    url: 'api/likes',
+    url: '/api/likes',
     params: {
         skip,
         limit
@@ -46,7 +46,7 @@ export const getLikedFilms = (skip, limit) => apiRequest({
 
 export const getFavoriteFilms = (skip, limit,id) => apiRequest({
     method: 'get',
-    url: 'api/favorites',
+    url: '/api/favorites',
     params: {
         skip,
         limit,
@@ -56,40 +56,40 @@ export const getFavoriteFilms = (skip, limit,id) => apiRequest({
 
 export const getFilmById = (id) => apiRequest({
     method: 'get',
-    url: `api/films/${id}`
+    url: `/api/films/${id}`
 });
 
 export const getUsers = () => apiRequest({
     method: 'get',
-    url: `api/user/all`
+    url: `/api/user/all`
 });
 
 export const follow = (id) => apiRequest({
     method: 'put',
-    url: `api/user/${id}`
+    url: `/api/user/${id}`
 });
 
 export const unFollow = (id) => apiRequest({
     method: 'delete',
-    url: `api/user/${id}`
+    url: `/api/user/${id}`
 });
 
 export const addFavorite = (id) => apiRequest({
     method: 'put',
-    url: `api/favorites/${id}`
+    url: `/api/favorites/${id}`
 });
 
 export const removeFavorite = (id) => apiRequest({
     method: 'delete',
-    url: `api/favorites/${id}`
+    url: `/api/favorites/${id}`
 });
 
 export const addLike = (id) => apiRequest({
     method: 'put',
-    url: `api/likes/${id}`
+    url: `/api/likes/${id}`
 });
 
 export const removeLike = (id) => apiRequest({
     method: 'delete',
-    url: `api/likes/${id}`
+    url: `/api/likes/${id}`
 });
